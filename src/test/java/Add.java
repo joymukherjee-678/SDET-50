7a27ea970d4878600a230e09fd24c36f67f34126
joymukherjee-678 <joymukherjee932@gmail.com>
2021-10-29 14:17:59 +0530

    comitted

---
+++ src/test/java/Add.java
@@ -0,0 +1,12 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Add {
+
+	@Test
+	public void add() {
+		int a=4,b=5;
+		
+		Reporter.log("result =", a+b, true);
+	}
+}
